80488fc64bdb0930cd75c37b527494abb94935f6
marciliodevjava <marciliodevjava@gmail.com>
2022-06-06 06:58:41 -0300

    Class Contador

--- src/basico/Contador.java
+++ src/basico/Contador.java
@@ -18,7 +18,9 @@ public class Contador extends Application{
 	public void start(Stage primaryStage) throws Exception {
 				
 		Label labelTitulo = new Label("Contador");
+		labelTitulo.getStyleClass().add("titulo");
 		Label labelNumero = new Label("0");
+		labelNumero.getStyleClass().add("numero");
 		
 		Button botaoDecremento = new Button("-");
 		botaoDecremento.setOnAction(e -> {
@@ -39,15 +41,18 @@ public class Contador extends Application{
 		boxBotoes.getChildren().add(botaoDecremento);
 		boxBotoes.getChildren().add(botaoIncremento);
 		
-		VBox boxPrincial = new VBox();
-		boxPrincial.setAlignment(Pos.CENTER);
-		boxPrincial.setSpacing(10);
+		VBox boxConteudo = new VBox();
+		boxConteudo.getStyleClass().add("conteudo");
+		boxConteudo.setSpacing(10);
+		boxConteudo.setAlignment(Pos.CENTER);
+		boxConteudo.getChildren().add(labelTitulo);
+		boxConteudo.getChildren().add(labelNumero);
+		boxConteudo.getChildren().add(boxBotoes);
 		
-		boxPrincial.getChildren().add(labelTitulo);
-		boxPrincial.getChildren().add(labelNumero);
-		boxPrincial.getChildren().add(boxBotoes);
-		
-		Scene cenaPrinciapal = new Scene(boxPrincial, 400, 400);
+		String caminhoCss = getClass().getResource("/basico/Contador.css").toExternalForm();
+		Scene cenaPrinciapal = new Scene(boxConteudo, 400, 400);
+		cenaPrinciapal.getStylesheets().add(caminhoCss);
+		cenaPrinciapal.getStylesheets().add("https://fonts.googleapis.com/css2?family=Oswald");
 		
 		primaryStage.setScene(cenaPrinciapal);
 		primaryStage.show();
