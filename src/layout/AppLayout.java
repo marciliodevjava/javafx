f93aa09c8408cb3b91fa1e39294a1023207c5e4b
marciliodevjava <marciliodevjava@gmail.com>
2022-06-07 08:42:47 -0300

    Class AppLayout

--- src/layout/AppLayout.java
+++ src/layout/AppLayout.java
@@ -2,7 +2,6 @@ package layout;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AppLayout extends Application {
@@ -10,16 +9,8 @@ public class AppLayout extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		VBox temp = new VBox();
-		temp.getChildren().add(new Quadrado());
-		temp.getChildren().add(new Quadrado());
-		temp.getChildren().add(new Quadrado());
-		temp.getChildren().add(new Quadrado());
-		temp.getChildren().add(new Quadrado());
-		temp.getChildren().add(new Quadrado());
-		temp.getChildren().add(new Quadrado());
-		Scene principal = new Scene(temp, 800, 600);
-
+		Scene principal = new Scene(new TesteAnchorPane(), 800, 600);
+		
 		primaryStage.setScene(principal);
 		primaryStage.setTitle("Gerenciadores de Layout");
 		primaryStage.show();
